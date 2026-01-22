768d601cfb49d44eafb27186087b050018c72303
ardi tono <arditono1992@gmail.com>
2026-01-23 02:25:50 +0700

    Deskripsi perubahan Anda di sini

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World Test! from GitHub Actions + Docker 🎉"
+    return "Hello,  GitHub Actions + Docker 🎉"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
